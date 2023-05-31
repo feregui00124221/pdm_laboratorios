05bd335b91d7eaf624bb469ceac7918dadaf592b
fernando00124221 <00124221@uca.edu.sv>
2023-05-31 15:24:13 -0600

    ADD: Laboratorio 10

---
+++ laboratorio10/Laboratorio10 - base/app/src/main/java/com/example/laboratorio05/data/model/CastModel.kt
@@ -0,0 +1,6 @@
+package com.example.laboratorio05.data.model
+
+import androidx.room.Entity
+
+@Entity(tableName = "cast_table", primaryKeys = ["movieId", "actorId"])
+data class CastModel (val movieId: Int, val actorId: Int)
